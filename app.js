c7d15d77eb15cb01195c457a18c0ec5bc16f618c
ch3rryl <cherryl@live.co.uk>
2024-02-15 15:03:27 +0000

    add styling to responsive nav drop-down

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const navList = document.getElementById("ul");
+const heroSection = document.getElementById("hero-section");
 
 function toggleButton() {
-    navList.classList.toggle('show')
+    navList.classList.toggle('show');
+
+    const isMenuVisible = navList.classList.contains('show');
+    
+    // If the menu is visible, move the hero section down
+    if (isMenuVisible) {
+        heroSection.style.transform = "translateY(220px)";
+    } else {
+        heroSection.style.transform = "translateY(0)";
+    }
 }
+
 
 hamburgerBtn.addEventListener('click',toggleButton);
